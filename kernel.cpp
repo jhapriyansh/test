a4a9847ab561645397a8e0beefaee7cd5fda5a63
PRIYANSHU KUMAR JHA <jhapriyanshu336@gmail.com>
2026-06-11 12:30:32 +0530

    kernel fix, printf implemented

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,12 @@
+void printf(char *str)
+{
+    unsigned short *VideoMemory = (unsigned short *)0xb8000;
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
 void kernelMain(void *multiboot_structure, unsigned int magicnumber)
 {
     printf("Hello World! --- https://0xpkj.tech");
